04c4e20ae7a2aa3e17dc7b82c2fd164fe6bd05c3
Anibal <anibal.siguenza1@gmail.com>
2020-12-13 19:52:33 +0000

    Make convolution NN

--- model.py
+++ model.py
@@ -1,15 +1,29 @@
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Lambda, MaxPooling2D, Dropout, Activation
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
 import numpy as np
 
 X_train = np.load('/opt/1/X_center.npy')
 y_train = np.load('/opt/1/y.npy')
 
 model = Sequential()
-model.add(Flatten(input_shape=(160, 320, 3)))
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+
+model.add(Convolution2D(6,5,5, activation='relu'))
+model.add(MaxPooling2D())
+# model.add(Dropout(.5))
+
+model.add(Convolution2D(6,5,5, activation='relu'))
+model.add(MaxPooling2D())
+# model.add(Dropout(.5))
+
+model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
 
 model.save('model.h5')
