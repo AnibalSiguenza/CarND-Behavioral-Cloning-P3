b9727c53b370e1e012039232596eb6b9fe9d4174
Anibal <anibal.siguenza1@gmail.com>
2021-01-12 17:12:17 +0000

    Add sample data to process

--- process_images_to_numpy.py
+++ process_images_to_numpy.py
@@ -13,7 +13,6 @@ def readLines(filename):
 
     return lines
 
-
 def saveImagesData(csv_filename, dir_to_save):
     lines = readLines(csv_filename)
 
@@ -25,15 +24,16 @@ def saveImagesData(csv_filename, dir_to_save):
     directions = []
 
     for line in lines:
-        center_filename = os.path.join(
-            image_dir, 'IMG', line[0].split('/')[-1])
-        left_filename = os.path.join(image_dir, 'IMG', line[1].split('/')[-1])
-        right_filename = os.path.join(image_dir, 'IMG', line[2].split('/')[-1])
+        if line[3] != 'steering':
+            center_filename = os.path.join(
+                image_dir, 'IMG', line[0].split('/')[-1])
+            left_filename = os.path.join(image_dir, 'IMG', line[1].split('/')[-1])
+            right_filename = os.path.join(image_dir, 'IMG', line[2].split('/')[-1])
 
-        center_images.append(cv2.imread(center_filename))
-        left_images.append(cv2.imread(left_filename))
-        right_images.append(cv2.imread(right_filename))
-        directions.append(float(line[3]))
+            center_images.append(cv2.imread(center_filename))
+            left_images.append(cv2.imread(left_filename))
+            right_images.append(cv2.imread(right_filename))
+            directions.append(float(line[3]))
 
     X_center = np.array(center_images)
     X_left = np.array(left_images)
@@ -53,10 +53,26 @@ dir_to_save = '/opt/'
 csv_files = [os.path.join(dp, f) for dp, dn, filenames in os.walk(
     image_root) for f in filenames if os.path.splitext(f)[1] == '.csv']
 
-i = 1
 for file in csv_files:
-    print('Processing images from file ', file)
-    dir_name =  dir_to_save + str(i)
+    dir_name =  dir_to_save + os.path.dirname(file).split('/')[-1]
+    if not(os.path.exists(dir_name)):
+        print('Processing images from file: ', file)
+        os.mkdir(dir_name)
+        saveImagesData(file, dir_name)
+        print('File saves on directory: ', dir_name)
+    else:
+        print('Images from file ', file, ' were already processed')
+        
+# loading course sample data
+file = './data/driving_log.csv'
+dir_name = '/opt/sample_data'
+csv_files = [os.path.join(dp, f) for dp, dn, filenames in os.walk(
+    image_root) for f in filenames if os.path.splitext(f)[1] == '.csv']   
+
+if not(os.path.exists(dir_name)):
     os.mkdir(dir_name)
+    print('Processing images from file: ', file)
     saveImagesData(file, dir_name)
-    i = i + 1
+    print('File saves on directory: ', dir_name)
+else:
+    print('Sample images already processed')
