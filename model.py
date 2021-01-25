04b2d0cde4e30c1e4869cae7c4975a6c5b88802b
Anibal <anibal.siguenza1@gmail.com>
2021-01-25 15:42:07 +0000

    Making full circuit!

--- model.py
+++ model.py
@@ -4,14 +4,21 @@ from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 import numpy as np
 
-X_train = np.load('/opt/center_1/X_center.npy')
+X_train = np.load('/opt/sample_data/X_center.npy')
+X_train = np.append(X_train, np.load('/opt/center_1/X_center.npy'), 0)
 X_train = np.append(X_train, np.load('/opt/center_2/X_center.npy'), 0)
 #X_train = np.append(X_train, np.load('/opt/bridge/X_center.npy'), 0)
 X_train = np.append(X_train, np.load('/opt/recovery_1/X_center.npy'), 0)
-y_train = np.load('/opt/center_1/y.npy')
+X_train = np.append(X_train, np.load('/opt/reverse_1/X_center.npy'), 0)
+#X_train = np.append(X_train, np.load('/opt/no_border_1/X_center.npy'), 0)
+
+y_train = np.load('/opt/sample_data/y.npy')
+y_train = np.append(y_train, np.load('/opt/center_1/y.npy'), 0)
 y_train = np.append(y_train, np.load('/opt/center_2/y.npy'), 0)
 #y_train = np.append(y_train, np.load('/opt/bridge/y.npy'), 0)
 y_train = np.append(y_train, np.load('/opt/recovery_1/y.npy'), 0)
+y_train = np.append(y_train, np.load('/opt/reverse_1/y.npy'), 0)
+#y_train = np.append(y_train, np.load('/opt/no_border_1/y.npy'), 0)
 
 # augmenting data by fliping
 X_train = np.append(X_train, np.flip(X_train,2), 0)
@@ -23,20 +30,28 @@ model = Sequential()
 model.add(Cropping2D(cropping=((70,25), (0,0)), input_shape=(160,320,3)))
 model.add(Lambda(lambda x: x / 255.0 - 0.5))
 
-model.add(Convolution2D(6,5,5, activation='relu'))
-model.add(MaxPooling2D())
+#model.add(Convolution2D(6,5,5, activation='relu'))
+#model.add(MaxPooling2D())
 #model.add(Dropout(.5))
 
-model.add(Convolution2D(6,5,5, activation='relu'))
-model.add(MaxPooling2D())
+#model.add(Convolution2D(6,5,5, activation='relu'))
+#model.add(MaxPooling2D())
 #model.add(Dropout(.5))
 
+model.add(Convolution2D(25,5,5, subsample=(2,2), activation='relu'))
+model.add(Convolution2D(36,5,5, subsample=(2,2), activation='relu'))
+model.add(Convolution2D(48,5,5, subsample=(2,2), activation='relu'))
+model.add(Convolution2D(64,3,3, activation='relu'))
+model.add(Convolution2D(64,3,3, activation='relu'))
+
+#model.add(Dropout(.4))
 model.add(Flatten())
-model.add(Dense(120))
-model.add(Dense(84))
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=2)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=3)
 
 model.save('model.h5')
